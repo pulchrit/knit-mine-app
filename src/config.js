31f31bedf085cc6040b8fbccb6112f0addcde068
Melissa Lafranchise <pulchrit@gmail.com>
2019-09-06 11:10:39 -0700

    removing part of deploy script that removed old aliases of knit-mine-app

--- src/config.js
+++ src/config.js
@@ -3,4 +3,7 @@ export default {
     API_ENDPOINT: 'https://quiet-shelf-50620.herokuapp.com/',
     TOKEN_KEY: 'AuthToken',
   }
+
+  // Storing the below here in case I need to replace the other package.json deploy script
+  //"deploy": "now --prod ./build && now rm knit-mine-app --safe --yes"
   
