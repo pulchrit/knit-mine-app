b8eef284fae8df01d8380e06c3f864b1d42e4880
Melissa Lafranchise <pulchrit@gmail.com>
2019-09-05 21:48:30 -0700

    deploy commit

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export default {
-    //API_ENDPOINT: 'http://localhost:8000/api',
-    API_ENDPOINT: 'http://localhost:8000/',
+    //API_ENDPOINT: 'http://localhost:8000/',
+    API_ENDPOINT: 'https://quiet-shelf-50620.herokuapp.com/',
     TOKEN_KEY: 'AuthToken',
   }
   
