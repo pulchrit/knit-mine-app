3f3bb726756cca4372ad9c85d3c6d0678d61a6b8
Melissa Lafranchise <pulchrit@gmail.com>
2019-09-06 09:00:47 -0700

    Fixed tests and deploy commit

--- src/__tests__/ProjectsStitchesPatterns.js
+++ src/__tests__/ProjectsStitchesPatterns.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
-import MyProjectPatterns from '../components/MyProjectPatterns';
+import ProjectsStitchesPatterns from '../components/ProjectsStitchesPatterns';
 
 
 it('renders without crashing', () => {
@@ -17,8 +17,8 @@ it('renders without crashing', () => {
                 gift_occasion: "Just because",
                 yarn: "Cascade Fibers, Superwash 220, Green Heather",
                 needles: "US 8, circular",
-                pattern_id: 5,
-                stitch_id: [1, 3, 5]
+                pattern: 5,
+                stitches: [1, 3, 5]
             },
             {
                 id: 2,
@@ -29,18 +29,25 @@ it('renders without crashing', () => {
                 gift_occasion: "Birthday 2019",
                 yarn: "Malabrigo, Rio, Azul",
                 needles: "US 4, circular",
-                pattern_id: 4,
-                stitch_id: [2]
+                pattern: 4,
+                stitches: [2]
             }
         ]
+    }
+
+    const props = {
+        match: {
+            path: '/my-projects'
+        }
     }
   
     const div = document.createElement('div');
 
     ReactDOM.render(
       <BrowserRouter>
-        <MyProjectPatterns  
+        <ProjectsStitchesPatterns  
             data={state.MY_PROJECTS}
+            match={props.match}
         />
       </BrowserRouter>, div);
     ReactDOM.unmountComponentAtNode(div);
